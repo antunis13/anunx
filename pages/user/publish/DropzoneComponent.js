2c685f3f6bf706ec4858c9f04ec55c5b7c372259
Richard Antunis <richardantunis4@gmail.com>
2023-06-28 11:16:59 -0300

    formik installation and configuration, title validate

--- pages/user/publish/DropzoneComponent.js
+++ pages/user/publish/DropzoneComponent.js
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { styled } from "styled-components"
 import { DeleteForever } from '@mui/icons-material'
-import { Box, IconButton, Typography } from "@mui/material"
+import { Box, IconButton, Typography, Container } from "@mui/material"
 
 import { useDropzone } from 'react-dropzone'
 
--- pages/user/publish/index.js
+++ pages/user/publish/index.js
@@ -12,8 +12,13 @@ import {
 } from '@mui/material'
 import { styled } from '@mui/system'
 
+import { Formik } from 'formik' 
+import { object, string, number, date, InferType } from 'yup'
+
+
 import TemplateDefault from '../../../src/templates/Default'
 import DropzoneComponent from './DropzoneComponent'
+
 
 
 
@@ -27,146 +32,182 @@ const StyledBox = styled(Box)(({ theme }) => ({
     padding: theme.spacing(3),
 }))
 
+const validationSchema = object({
+    title: string()
+        .min(6, 'Escreva um título maior')
+        .max(100, 'Título muito grande')
+        .required('Campo obrigatório'),
+})
+
 
 const Publish = () => {
 
     return(
         <TemplateDefault>
-            <Container maxWidth='sm'>
-                <Typography component='h1' variant='h2' align='center' color='textPrimary'>
-                    Publicar Anúncio
-                </Typography>
-                <Typography component='h5' variant='h5' align='center' color='textPrimary'>
-                    Quanto mais detalhado melhor!
-                </Typography>
-            </Container>
+            <Formik
+                initialValues={{
+                    title: ''
+                }}
+                validationSchema={validationSchema}
+                onSubmit={(values) => {
+                    console.log('enviou o form', values)
+                }}
+            >
+                {
+                    ({
+                        values,
+                        errors,
+                        handleChange,
+                        handleSubmit,
+                    })=>{
+                        console.log(errors)
+                        return(
+                            <form onSubmit={handleSubmit}> 
+                                <Container maxWidth='sm'>
+                                    <Typography component='h1' variant='h2' align='center' color='textPrimary'>
+                                        Publicar Anúncio
+                                    </Typography>
+                                    <Typography component='h5' variant='h5' align='center' color='textPrimary'>
+                                        Quanto mais detalhado melhor!
+                                    </Typography>
+                                </Container>
 
-            <BoxContainer maxWidth='md'>
-                <StyledBox>
-                    <Typography component='h6' variant='h6' color='textPrimary'>
-                        Título do Anúncio
-                    </Typography>
-                    <TextField 
-                        label='ex.: Bicicleta Aro 29 Shimano'
-                        size='small'
-                        fullWidth
-                        variant='standard'
-                    />
-                    <br /><br/>
-                    <Typography component='h6' variant='h6' color='textPrimary'>
-                        Categoria
-                    </Typography>
-                    <Select
-                        native
-                        value=""
-                        fullWidth
-                        onChange={() => {}}
-                        inputProps={{
-                            name: 'age',
-                        }}
-                    >
-                        <option value="">Selecione</option>
-                        <option value={1}>Bebê e Criança</option>
-                        <option value={2}>Agricultura</option>
-                        <option value={3}>Moda</option>
-                        <option value={3}>Carros, Motos e Barcos</option>
-                        <option value={3}>Serviços</option>
-                        <option value={3}>Lazer</option>
-                        <option value={3}>Animais</option>
-                        <option value={3}>Móveis, Casa e Jardim</option>
-                        <option value={3}>Imóveis</option>
-                        <option value={3}>Equipamentos e Ferramentas</option>
-                        <option value={3}>Celulares e Tablets</option>
-                        <option value={3}>Esporte</option>
-                        <option value={3}>Tecnologia</option>
-                        <option value={3}>Emprego</option>
-                        <option value={3}>Outros</option>
-                    </Select>
-                </StyledBox>
-            </BoxContainer>
+                                <BoxContainer maxWidth='md'>
+                                    <StyledBox>
+                                        <Typography component='h6' variant='h6' color='textPrimary'>
+                                            Título do Anúncio
+                                        </Typography>
+                                        <TextField 
+                                            name='title'
+                                            value={values.title}
+                                            onChange={handleChange}
+                                            label='ex.: Bicicleta Aro 29 Shimano'
+                                            size='small'
+                                            fullWidth
+                                            variant='standard'
+                                            error={errors.title}
+                                            helperText={errors.title}
+                                        />
+                                        <br /><br/>
+                                        <Typography component='h6' variant='h6' color='textPrimary'>
+                                            Categoria
+                                        </Typography>
+                                        <Select
+                                            native
+                                            value=""
+                                            fullWidth
+                                            onChange={() => {}}
+                                            inputProps={{
+                                                name: 'age',
+                                            }}
+                                        >
+                                            <option value="">Selecione</option>
+                                            <option value={1}>Bebê e Criança</option>
+                                            <option value={2}>Agricultura</option>
+                                            <option value={3}>Moda</option>
+                                            <option value={3}>Carros, Motos e Barcos</option>
+                                            <option value={3}>Serviços</option>
+                                            <option value={3}>Lazer</option>
+                                            <option value={3}>Animais</option>
+                                            <option value={3}>Móveis, Casa e Jardim</option>
+                                            <option value={3}>Imóveis</option>
+                                            <option value={3}>Equipamentos e Ferramentas</option>
+                                            <option value={3}>Celulares e Tablets</option>
+                                            <option value={3}>Esporte</option>
+                                            <option value={3}>Tecnologia</option>
+                                            <option value={3}>Emprego</option>
+                                            <option value={3}>Outros</option>
+                                        </Select>
+                                    </StyledBox>
+                                </BoxContainer>
 
-            <BoxContainer maxWidth='md'>
-                <StyledBox>
-                    <Typography component='h6' variant='h6' color='textPrimary'>
-                        Imagens
-                    </Typography>
-                    <Typography component='div' variant='body2' color='textPrimary'>
-                        A primeira imagem é a foto principal do seu anúncio.
-                    </Typography>
-                    <DropzoneComponent/>
-                </StyledBox>
-            </BoxContainer>
+                                <BoxContainer maxWidth='md'>
+                                    <StyledBox>
+                                        <Typography component='h6' variant='h6' color='textPrimary'>
+                                            Imagens
+                                        </Typography>
+                                        <Typography component='div' variant='body2' color='textPrimary'>
+                                            A primeira imagem é a foto principal do seu anúncio.
+                                        </Typography>
+                                        <DropzoneComponent/>
+                                    </StyledBox>
+                                </BoxContainer>
 
-            <BoxContainer maxWidth='md'>
-                <StyledBox>
-                    <Typography component='h6' variant='h6' color='textPrimary'>
-                        Descrição 
-                    </Typography>
-                    <Typography component='div' variant='body2' color='textPrimary'>
-                       Escreva os detalhes do que está vendendo.
-                    </Typography>
-                    <TextField 
-                        multiline
-                        rows={6}
-                        variant='outlined'
-                        fullWidth
-                    />
-                </StyledBox>
-            </BoxContainer>
+                                <BoxContainer maxWidth='md'>
+                                    <StyledBox>
+                                        <Typography component='h6' variant='h6' color='textPrimary'>
+                                            Descrição 
+                                        </Typography>
+                                        <Typography component='div' variant='body2' color='textPrimary'>
+                                        Escreva os detalhes do que está vendendo.
+                                        </Typography>
+                                        <TextField 
+                                            multiline
+                                            rows={6}
+                                            variant='outlined'
+                                            fullWidth
+                                        />
+                                    </StyledBox>
+                                </BoxContainer>
 
-            <BoxContainer maxWidth='md'>
-                <StyledBox>
-                    <Typography component='h6' variant='h6' color='textPrimary'>
-                        Preço 
-                    </Typography>
-                    <br />
-                    <FormControl fullWidth variant='outlined'>
-                        <FormLabel>
-                            Valor
-                        </FormLabel>
-                        <OutlinedInput 
-                            onChange={() => {}}
-                            startAdornment={<InputAdornment position='start'>R$</InputAdornment>}
-                        />
-                    </FormControl>
-                </StyledBox>
-            </BoxContainer>
+                                <BoxContainer maxWidth='md'>
+                                    <StyledBox>
+                                        <Typography component='h6' variant='h6' color='textPrimary'>
+                                            Preço 
+                                        </Typography>
+                                        <br />
+                                        <FormControl fullWidth variant='outlined'>
+                                            <FormLabel>
+                                                Valor
+                                            </FormLabel>
+                                            <OutlinedInput 
+                                                onChange={() => {}}
+                                                startAdornment={<InputAdornment position='start'>R$</InputAdornment>}
+                                            />
+                                        </FormControl>
+                                    </StyledBox>
+                                </BoxContainer>
 
-            <BoxContainer maxWidth='md'>
-                <StyledBox>
-                    <Typography component='h6' variant='h6' color='textPrimary' gutterBottom>
-                        Dados de Contato
-                    </Typography>
-                    <TextField 
-                        label='Nome'
-                        variant='outlined'
-                        size='small'
-                        fullWidth
-                    />
-                    <br/> <br/>
-                    <TextField 
-                        label='E-mail'
-                        variant='outlined'
-                        size='small'
-                        fullWidth
-                    />
-                    <br/> <br/>
-                    <TextField 
-                        label='Telefone'
-                        variant='outlined'
-                        size='small'
-                        fullWidth
-                    />
-                </StyledBox>
-            </BoxContainer>
+                                <BoxContainer maxWidth='md'>
+                                    <StyledBox>
+                                        <Typography component='h6' variant='h6' color='textPrimary' gutterBottom>
+                                            Dados de Contato
+                                        </Typography>
+                                        <TextField 
+                                            label='Nome'
+                                            variant='outlined'
+                                            size='small'
+                                            fullWidth
+                                        />
+                                        <br/> <br/>
+                                        <TextField 
+                                            label='E-mail'
+                                            variant='outlined'
+                                            size='small'
+                                            fullWidth
+                                        />
+                                        <br/> <br/>
+                                        <TextField 
+                                            label='Telefone'
+                                            variant='outlined'
+                                            size='small'
+                                            fullWidth
+                                        />
+                                    </StyledBox>
+                                </BoxContainer>
 
-            <BoxContainer maxWidth='md'>
-                <Box textAlign='right'>
-                    <Button variant='contained' color='primary'>
-                        Publicar Anúncio
-                    </Button>
-                </Box>
-            </BoxContainer>
+                                <BoxContainer maxWidth='md'>
+                                    <Box textAlign='right'>
+                                        <Button type='submit' variant='contained' color='primary'>
+                                            Publicar Anúncio
+                                        </Button>
+                                    </Box>
+                                </BoxContainer>
+                            </form>
+                        )
+                    }
+                }
+            </Formik>
         </TemplateDefault>
     )
 }
